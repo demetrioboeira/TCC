81b36c997159386b0c9a24f1f926d2de7dd36f67
demetrioboeira <51182514+demetrioboeira@users.noreply.github.com>
2022-08-28 20:24:20 -0300

    Add files via upload

--- TCC3.py
+++ TCC3.py
@@ -1,53 +1,114 @@
 import dns.resolver
 import subprocess
+import concurrent.futures
 import os
 import re
 #NS = dns.resolver.resolve(domain, 'NS')
-NSlist = []
+
 Domainlist = []
-with open ('trancolist.txt' , 'r') as dominios:
+
+
+with open ('trancolistreduzida.txt' , 'r') as dominios:
 	for dominio in dominios:
 		dominio = ''.join([i for i in dominio if not i.isdigit()])
 		dominio = dominio[1:]
 		Domainlist.append(dominio)
 
 #print (Domainlist)
+NSlist = []
 
-	
-for dominio in Domainlist:
+def getNSrecords (dominio):
+	domainNSList = []
 	dominio = dominio[:-1]
-	print (dominio)
+	#print (dominio)
+	
 	try:	
 		NS = dns.resolver.resolve(dominio, 'NS')
+		#print (NS)
 		for answer in NS.response.answer:
+			#print (answer)
 			for item in answer.items:
-				NSlist.append(item.to_text())
+				#print (item)
+				#global NSlist
+				domainNSList.append(item.to_text())
+				#print (NSlist)
+		#print (domainNSList)
+		return domainNSList
 	except:
-		continue
+		pass
 
-#print(Domainlist)
+	
+with concurrent.futures.ProcessPoolExecutor(max_workers=200) as executor:
+	for result in executor.map(getNSrecords, Domainlist):
+		try:
+			for item in result:
+				NSlist.append(item)
+		except:
+			pass
+		
+
 print (NSlist)
 
 
 Arecords = []
-AAAArecords = []
-for nsrecord in NSlist:
-	   nsrecord = nsrecord[:-1]
-	   try:
-	   	teste = subprocess.check_output(["nslookup", nsrecord])
-	   	first, *middle, last = teste.split()
-	   	try:
-	   		AAAArecords.append(last.decode("utf-8"))
-	   		Arecords.append(middle[8].decode("utf-8"))
-	   	except:
-	   		Arecords.append(last.decode("utf-8"))
-	   except:
-	   	continue	
 
-#print ("NS RECORDS:")
-#print (NSlist)
-#print ("A RECORDS:")
-#print (Arecords)
+
+def getArecords (nsrecord):
+	nsrecord = nsrecord[:-1]
+	Arecord = ''
+	try:
+		teste = subprocess.check_output(["nslookup", nsrecord])
+		first, *middle, last = teste.split()
+		try:
+	   		Arecord = middle[8].decode("utf-8")
+		except:
+	   		Arecord = last.decode("utf-8")
+		#print (Arecord)
+		return Arecord
+	except:
+		pass
+		
+with concurrent.futures.ProcessPoolExecutor(max_workers=200) as executor:
+	for result in executor.map(getArecords, NSlist):
+		if result != None:
+			print (result)
+			Arecords.append(result)
+		#else:
+			#print ('Era none')	
+
+print ("A RECORDS:")
+print (Arecords)
+
+
+AAAArecords = []		
+def getAAAArecords (nsrecord):
+	nsrecord = nsrecord[:-1]
+	AAAArecord = ''
+	Arecord = ''
+	try:
+		teste = subprocess.check_output(["nslookup", nsrecord])
+		first, *middle, last = teste.split()
+		try:
+			Arecord = middle[8].decode("utf-8")
+			AAAArecord = last.decode("utf-8")
+		except:
+	   		pass
+	   	
+		return AAAArecord
+	except:
+		pass
+
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=200) as executor:
+	for result in executor.map(getAAAArecords, NSlist):
+		if result != None:
+			print (result)
+			AAAArecords.append(result)
+		#else:
+			#print ('Era none')
+
+print ("AAAA RECORDS:")
+print (AAAArecords)
 
 with open ('IpList.txt', 'w') as file:
 	file.write("begin \n")
@@ -59,8 +120,8 @@ with open ('IpList.txt', 'w') as file:
 		file.write("\n")
 	file.write("end")	
 
-'''
 
+'''
 comand = "netcat whois.cymru.com 43 <IpList.txt | sort -n > listaordenada"
 
 
