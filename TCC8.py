0d9b21c572a5f7cc738c0e2bc92bac8e42529185
demetrioboeira <51182514+demetrioboeira@users.noreply.github.com>
2022-11-15 13:58:07 -0300

    Update and rename TCC7.py to TCC8.py

--- TCC8.py
+++ TCC8.py
@@ -3,24 +3,33 @@ import subprocess
 import concurrent.futures
 import os
 import re
+import glob
+import shutil
 from datetime import date
-
 import downloader
 
 iphash = {}
 ip6hash = {}
 asnhash = {}
 
+pastanova = "Resultados" + str(date.today())
+os.mkdir(pastanova)
 
-with open ("ip2as.txt", "r") as ip:
+arquivo = glob.glob('*.pfx2as')
+arquivo = str(arquivo)
+arquivo = arquivo[:-2]
+arquivo = arquivo[2:]
+
+
+with open (arquivo, "r") as ip:
 	for line in ip:
 		token = line.split('	')
 		rede = token[0]
 		asn = token[2]
 		iphash[rede] = asn
-		
-		
-#print (iphash['185.241.125.0'])
+
+os.remove(arquivo)			
+
 
 with open ("ip62as.txt", "r") as ip6:
 	for line in ip6:
@@ -29,56 +38,43 @@ with open ("ip62as.txt", "r") as ip6:
 		asn = token[2]
 		ip6hash[rede] = asn
 		
-#print (ip6hash['2c0f:2e00:1a0::'])
 
-with open ("as2org.txt", "r") as asn:
-	for line in asn:
+
+arquivo = glob.glob('*.as-org2info.txt')
+arquivo = str(arquivo)
+arquivo = arquivo[:-2]
+arquivo = arquivo[2:]
+
+
+with open (arquivo, "r") as asn:
+	for line in asn:		
 		token = line.split('|')
-		asn = token[0]
-		org = token[2]
-		asnhash[asn] = org
-'''	
-with open ("Ipv4List.txt 2022-09-21", "r") as listaip:
-	for line in listaip:
-		token = line.split('.')
-		token[3] = 0
-		mascara = str(token[0]) + "." + str(token[1]) + "." + str(token[2]) + "." + str(token[3])
 		try:
-			print (asnhash[iphash[mascara][:-1]])
+			asn = token[0]
+			org = token[2]
+			asnhash[asn] = org
 		except:
-			print ("mascara de rede " + mascara + " não encontrada na lista" )
-			
-with open ("Ipv6List.txt 2022-09-21", "r") as listaip6:
-	for line in listaip6:
-		token = line.split(':')
-		mascara = str(token[0]) + ":" + str(token[1]) + ":" + str(token[2]) + "::"
-		try:
-			print(asnhash[ip6hash[mascara][:-1]])
-		except:
-			print ("mascara de rede " + mascara + " não encontrada na lista" )
-'''
-
+			pass
+os.remove(arquivo)
+	
 Domainlist = []
 
 
-class Dominio:
-	def __init__(self, nome, nsrecords, arecords, aaaarecords, empresa):
-		self.nome = nome
-		self.nsrecords = nsrecords
-		self.arecords = arecords
-		self.aaaarecords = aaaarecords
-		self.empresa = empresa
-	
+arquivo = glob.glob('*.csv')
+arquivo = str(arquivo)
+arquivo = arquivo[:-2]
+arquivo = arquivo[2:]
 
 
-with open ('trancolistreduzida.txt' , 'r') as dominios:
+with open (arquivo, 'r') as dominios:
 	for dominio in dominios:
 		dominio = ''.join([i for i in dominio if not i.isdigit()])
 		dominio = dominio[1:]
 		Domainlist.append(dominio)
+		
 
-#print (Domainlist)
-NSlist = []
+
+shutil.move ("/home/Demetrio/Documentos/TCC/" + arquivo, "/home/Demetrio/Documentos/TCC/" + pastanova)
 
 def getArecords (nsrecord):
 	nsrecord = nsrecord[:-1]
@@ -87,7 +83,6 @@ def getArecords (nsrecord):
 		query = dns.resolver.resolve(nsrecord, 'A')
 		for item in query:
 			Arecord.append(item.to_text())
-		#print (Arecord)
 		return Arecord
 		
 	except:
@@ -101,7 +96,6 @@ def getAAAArecords (nsrecord):
 		query = dns.resolver.resolve(nsrecord, 'AAAA')
 		for item in query:
 			AAAArecord.append(item.to_text())
-		#print (AAAArecord)
 		return AAAArecord
 		
 	except:
@@ -116,19 +110,12 @@ def querydominio (dominio):
 	listaempresas = []
 	tempA = []
 	tempAAAA = []
-	#print (dominio)
 	
 	try:	
 		NS = dns.resolver.resolve(dominio, 'NS')
-		#print (NS)
 		for answer in NS.response.answer:
-			#print (answer)
 			for item in answer.items:
-				#print (item)
-				#global NSlist
 				domainNSList.append(item.to_text())
-				#print (NSlist)
-		#print (domainNSList)
 		
 		
 		for nsrecord in domainNSList:
@@ -163,14 +150,12 @@ def querydominio (dominio):
 			except:
 				pass
 		
-		#print (listaempresas)
+		
 		if len(listaempresas) != 0:
-			#print (listaempresas)
 			try:
-				
 				print (dominio, domainNSList, Arecords, AAAArecords, listaempresas)
 				
-			
+				listaempresas = str(listaempresas)
 				with open ('Medição dia' + str(date.today()), 'a') as file:
 						file.write('#######################################################')
 						file.write("\n")
@@ -191,20 +176,40 @@ def querydominio (dominio):
 				pass
 		else:
 			with open ('Erro Medição dia' + str(date.today()), 'a') as file:
-				file.write('nao encontrado a empresa do dominio' + '' + dominio)
+				file.write('nao encontrado a empresa do dominio' + ' ' + dominio)
 				file.write('\n')
-
-		
-		
-		#return domainNSList
-		#return dominio1
 	except:
 		pass
 		
-		
-
 	
+
 with concurrent.futures.ProcessPoolExecutor(max_workers=200) as executor:
 	for result in executor.map(querydominio, Domainlist):
 		pass
-	
+
+shutil.move ("/home/Demetrio/Documentos/TCC/Medição dia" + str(date.today()), "/home/Demetrio/Documentos/TCC/" + pastanova)
+shutil.move ("/home/Demetrio/Documentos/TCC/Erro Medição dia" + str(date.today()), "/home/Demetrio/Documentos/TCC/" + pastanova)
+
+
+concentracao = {}
+controle = 1
+
+with open ('Medição dia' + str(date.today()), 'r') as arquivo:
+	for line in arquivo:
+		line = str(line)		
+		if controle == 11:
+			if line in concentracao.keys():
+				concentracao[line] = concentracao[line] + 1
+			else:
+				concentracao[line] = 1
+			controle = 1
+		else:	
+			controle = controle + 1
+
+
+
+for item in sorted (concentracao, key = concentracao.get, reverse=True):
+	with open ('Concentracao dia' + str(date.today()), 'w') as file:
+		file.write(item, concentracao[item])
+
+shutil.move ("/home/Demetrio/Documentos/TCC/Concentracao dia" + str(date.today()), "/home/Demetrio/Documentos/TCC/" + pastanova)
